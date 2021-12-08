00ce75297b7e8f3cde1f7e07f9971b85088407d1
Kevin Welsh <kevinwelsh132@gmail.com>
2021-12-07 22:53:07 -0600

    Ohh just those changes? Not that many files.

--- src/App.svelte
+++ src/App.svelte
@@ -7,10 +7,16 @@
 	let whiteGraph, blackGraph;
 	let DEPTH_VALUES = [1,5,10,15,20];
 	let QUERY_INTERVAL = 10;
-	let URL_ROOT = "https://sharp-api-qkzpapi3la-ue.a.run.app/sharpness";
+	let URL_ROOT = "https://sharp-api-qkzpapi3la-ue.a.run.app";
 
 	let FENS = [];
+	let fen_index = 0;
 	let board;
+
+	let game_options = [];
+	let game_number;
+
+	let whiteChart, blackChart;
 
 	let COLORS = [
 		'#377eb8',
@@ -20,24 +26,6 @@
 		'#ff7f00',
 		'black'
 	]
-
-	function legendClickHandler(e, legendItem, legend) {
-		let index = legendItem.datasetIndex;
-		console.log(e);
-		console.log(legendItem);
-		console.log(legend);
-
-		let ci = legend.chart;
-		[
-			ci.getDatasetMeta(index-1),
-			ci.getDatasetMeta(index),
-			ci.getDatasetMeta(index+1)
-		].forEach(function(meta) {
-			meta.hidden = meta.hidden === null ? !ci.data.datasets[index].hidden : null;
-		});
-		ci.update();
-	};
-
 
 	let whiteData = {
 		labels : [],
@@ -75,6 +63,110 @@
 		return [white_sharpness, black_sharpness, white_eval, black_eval, fens];
 	};
 
+	function updateBoard(fen) {
+		if (board === undefined){
+			return;
+		}
+		while (board.firstChild) {
+			board.removeChild(board.firstChild);
+		}
+		Chessboard(board.id, fen);
+	};
+
+	function updateGraph(game_number) {
+		console.log(game_number)
+		if (game_number === undefined || whiteChart === undefined || blackChart === undefined) {
+			return;
+		}
+		let query_promises = [];
+		for (let d of DEPTH_VALUES) {
+			query_promises.push(
+				fetch(`${URL_ROOT}/sharpness?game=${game_number}&depth=${d}`)
+					.then(r => {
+							if (r.body) {
+								return r.json();
+							}
+							return {};
+						})
+			);
+		}
+
+		Promise.all(query_promises).then((query_results) => {
+			whiteData.datasets = []
+			blackData.datasets = []
+			
+			let whiteEvaluation = [];
+			let blackEvaluation = [];
+			for (let j = 0; j < query_results.length; j++) {
+				let result = query_results[j];
+				let [white_metric, black_metric, white_eval, black_eval, fens] = parseData(result);
+				whiteEvaluation = white_eval;
+				blackEvaluation = black_eval;
+				FENS = fens;
+				whiteData.labels = [];
+				blackData.labels = [];
+				for (let i = 0; i < white_metric.length; i++) {
+					whiteData.labels.push(i);
+					blackData.labels.push(i);
+				}
+				
+				whiteData.datasets.push({
+					data: white_metric,
+					tension: 0.4,
+					label: `Depth ${DEPTH_VALUES[j]}`,
+					hidden: j < DEPTH_VALUES.length-1,
+					order: 10 - j
+				});
+				blackData.datasets.push({
+					data: black_metric,
+					tension: 0.4,
+					label: `Depth ${DEPTH_VALUES[j]}`,
+					hidden: j < DEPTH_VALUES.length-1,
+					order: 10 - j
+				});
+			}
+			
+			whiteData.datasets.push({
+				data: whiteEvaluation,
+				tension: 0,
+				label: `Evaluation`,
+				hidden: true,
+				borderDash: [2, 2],
+				order: -10
+			});
+			blackData.datasets.push({
+				data: blackEvaluation,
+				tension: 0,
+				label: `Evaluation`,
+				hidden: true,
+				borderDash: [2, 2],
+				order: -10
+			});
+
+			
+			fen_index = FENS.length - 1;
+			updateBoard(FENS[fen_index]);
+			whiteChart.update();
+			blackChart.update();
+		});
+	};
+
+	function onKeyDown(e) {
+		console.log(e)
+		if (e.keyCode == 37) {
+			if (fen_index > 0) {
+				fen_index = fen_index - 1;
+			}
+		} else if (e.keyCode == 39) {
+			if (fen_index < FENS.length) {
+				fen_index = fen_index + 1;
+			}
+		}
+	};
+	
+	$: updateGraph(game_number);
+	$: updateBoard(FENS[fen_index]);
+
     onMount(() => {
 		let whiteChartOptions = {
 			elements: {
@@ -95,7 +187,7 @@
 				}
 			},
 		};
-		let whiteChart = new Chart(whiteGraph, {
+		whiteChart = new Chart(whiteGraph, {
 			type: 'line',
 			data: whiteData,
 			options: whiteChartOptions,
@@ -103,95 +195,34 @@
 		let blackChartOptions = JSON.parse(JSON.stringify(whiteChartOptions));
 		blackChartOptions.plugins.title.text = 'Black Sharpness';
 		
-		let blackChart = new Chart(blackGraph, {
+		blackChart = new Chart(blackGraph, {
 			type: 'line',
 			data: blackData,
 			options: blackChartOptions,
 		});
 
 
-		let updateGraph = () => {
-			let query_promises = [];
-			for (let d of DEPTH_VALUES) {
-				let url = URL_ROOT + `?game=${6}&depth=${d}`;
-				query_promises.push(
-					fetch(url)
-						.then(
-							r => {
-								if (r.body) {
-									return r.json();
-								}
-								return {};
-							})
-				);
-			}
-
-			Promise.all(query_promises).then((query_results) => {
-				whiteData.datasets = []
-				blackData.datasets = []
-				
-				let whiteEvaluation = [];
-				let blackEvaluation = [];
-				for (let j = 0; j < query_results.length; j++) {
-					let result = query_results[j];
-					let [white_metric, black_metric, white_eval, black_eval, fens] = parseData(result);
-					whiteEvaluation = white_eval;
-					blackEvaluation = black_eval;
-					FENS = fens;
-					whiteData.labels = [];
-					blackData.labels = [];
-					for (let i = 0; i < white_metric.length; i++) {
-						whiteData.labels.push(i);
-						blackData.labels.push(i);
-					}
-					
-					whiteData.datasets.push({
-						data: white_metric,
-						tension: 0.4,
-						label: `Depth ${DEPTH_VALUES[j]}`,
-						hidden: j < DEPTH_VALUES.length-1,
-						order: 10 - j
-					});
-					blackData.datasets.push({
-						data: black_metric,
-						tension: 0.4,
-						label: `Depth ${DEPTH_VALUES[j]}`,
-						hidden: j < DEPTH_VALUES.length-1,
-						order: 10 - j
-					});
-				}
-				
-				whiteData.datasets.push({
-					data: whiteEvaluation,
-					tension: 0,
-					label: `Evaluation`,
-					hidden: true,
-      				borderDash: [2, 2],
-					order: -10
-				});
-				blackData.datasets.push({
-					data: blackEvaluation,
-					tension: 0,
-					label: `Evaluation`,
-					hidden: true,
-      				borderDash: [2, 2],
-					order: -10
-				});
-
-				Chessboard(board.id, FENS[FENS.length - 1]);
-				whiteChart.update();
-				blackChart.update();
-			});
-		};
-		updateGraph();
-		// 	//setInterval(updateGraph, QUERY_INTERVAL * 1000);
-    });
+		fetch(`${URL_ROOT}/games`).then(async (r) => {
+			game_options = (await r.json()).sort((a,b) => a-b);
+			game_number = game_options[game_options.length - 1];
+		});
+	});
 	
 </script>
 
-
+<svelte:window on:keydown={onKeyDown}/>
 <main>
-	<graph-panel class = "graph-panel">
+	<div class="row">
+		<label for="Game">WCC 2021 Game:</label>
+		<select name="Game" bind:value={game_number}>
+			{#each game_options as game}
+				<option value="{game}">{game}</option>
+			{/each}
+
+		</select>
+	</div>
+	<div class="break"></div>
+	<div class="graphs">
 		<div>
 			<canvas id="white-graph" bind:this={whiteGraph}>
 			</canvas>
@@ -201,30 +232,43 @@
 			<canvas id="black-graph" bind:this={blackGraph}>
 			</canvas>
 		</div>
-	</graph-panel>
+	</div>
 
-	<boardContainer>
-		<div id="board" bind:this={board} style="width: 400px;"></div>
-	</boardContainer>
+	<div id="board" bind:this={board} style="width: 400px;"></div>
 </main>
 
 
 <style>
-	graph-panel {
-		width: 500px;
+	/* graph-panel {
+		width: 650px;
 		display: flex;
 		flex-direction: column;
 		margin-right: 50px;
-	}
-	graph-panel > div {
-		max-width: 600px;
+	} */
+	.graphs {
+		width: 650px;
+		margin-left: auto;
 	}
 	main {
 		display: flex;
-		flex-direction: row;
+		flex-wrap: wrap;
 	}
-	boardContainer {
-		margin-bottom: auto;
+	.row {
+		margin-left: auto;
+		margin-right: auto;
+	}
+	#board {
 		margin-top: auto;
+		margin-bottom: auto;
+		margin-left: 40px;
+		margin-right: auto;
+	}
+	.break {
+		flex-basis: 100%;
+		height: 0;
+	}
+	.graphs-and-board {
+		display: flex;
+		flex-direction: row;
 	}
 </style>
